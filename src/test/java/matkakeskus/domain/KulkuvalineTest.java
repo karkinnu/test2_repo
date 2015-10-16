f6e30fc782056e4894ff309ca8d24d696aa47dfa
thor <kajkinnu@gmail.com>
2015-10-16 15:11:29 +0300

    jep

--- src/test/java/matkakeskus/domain/KulkuvalineTest.java
+++ src/test/java/matkakeskus/domain/KulkuvalineTest.java
@@ -78,6 +78,7 @@ public class KulkuvalineTest {
 
 	@Test
 	public void etsiMatkustaja() {
+		// just Jenkins test
 		Kulkuvaline kulkuvaline = new Kulkuvaline(10, "1");
 		boolean result = kulkuvaline.lisaaMatkustaja(new Matkustaja("Pelle Peloton"));
 		result = kulkuvaline.lisaaMatkustaja(new Matkustaja("Roope Ankka"));
